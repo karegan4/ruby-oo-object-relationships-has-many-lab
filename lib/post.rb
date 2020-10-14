9a088c5fc3be93534581b4a5a507fd495a09e73b
Kaitlin Regan <karegan@gmail.com>
2020-10-14 10:24:08 -0400

    done

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+
+    attr_accessor :title, :author, :author_name
+
+    @@all = []
+    def initialize(title)
+        
+        @title = title
+        @@all <<self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        @author ? @author.name : nil
+    end
+end
